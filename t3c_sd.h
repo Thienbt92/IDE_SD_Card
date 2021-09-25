b939c78c81fbccd34140e4f26fd28c335a01fecf
BuiThien <bui.trong.thien.92@gmail.com>
2021-09-26 00:34:50 +0700

    Update 26/09/2021 00:34

--- t3c_sd.h
+++ t3c_sd.h
@@ -1,10 +1,14 @@
+#include "main.h"
+#include "SD.h"
+#include "FS.h"
+
 class mSD_Class
 {
 private:
     /* data */
     bool debugView = false;
 public:
-    bool mSD_begin(uint8_t ssPin, SPIClass &spi, uint32_t frequency, const char * mountpoint, uint8_t max_files,bool format_if_empty);
+    bool mSD_begin(uint8_t ssPin=SS, SPIClass &spi=SPI, uint32_t frequency=4000000, const char * mountpoint="/sd", uint8_t max_files=5, bool format_if_empty=false);
     uint8_t mSD_cardType();
     void mSD_listDirSerial(const char * dirname, uint8_t levels);
     bool mSD_createDir(const char * path);
@@ -17,5 +21,6 @@ public:
     bool mSD_open(File  *file,const char * path,const char* mode);
     bool mSD_read(File  *file,uint8_t *buffer,uint32_t _size);
     bool mSD_seek(File *file,uint32_t pos);
-    void mSD_Class::mSD_close(File  *file);
+    void mSD_close(File  *file);
 };
+
